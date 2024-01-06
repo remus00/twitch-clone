db5047dc2391a76a9c92d94ef15a420113bea226
remus00 <135813551+remus00@users.noreply.github.com>
2024-01-06 17:58:06 +0100

    Adding authentication with CLERK

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
-import Image from 'next/image';
+import { UserButton } from '@clerk/nextjs';
 
 export default function Home() {
-    return <main className="text-red-500 font-bold">Ciao</main>;
+    return (
+        <main className="flex flex-col gap-y-4">
+            <h1>Dashboard</h1>
+            <UserButton afterSignOutUrl="/" />
+        </main>
+    );
 }
