1c87e041c9a8b34a7debd435ef3f958fba7fa3ba
remus00 <135813551+remus00@users.noreply.github.com>
2024-01-06 18:18:24 +0100

    auth layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,12 @@
+import { Logo } from './_components/logo';
+
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
-    return <div className="w-full h-full flex items-center justify-center">{children}</div>;
+    return (
+        <div className="w-full h-full flex flex-col items-center justify-center space-y-6">
+            <Logo />
+            {children}
+        </div>
+    );
 };
 
 export default AuthLayout;
