409144b5b680398b0766eac7158ca3b27ac6d576
remus00 <135813551+remus00@users.noreply.github.com>
2024-02-11 16:20:07 +0100

    block service

--- app/(browse)/[username]/page.tsx
+++ app/(browse)/[username]/page.tsx
@@ -1,3 +1,4 @@
+import { isBlockedByUser } from '@/lib/block-service';
 import { isFollowingUser } from '@/lib/follow-service';
 import { getUserByUserName } from '@/lib/user-service';
 import { notFound } from 'next/navigation';
@@ -17,12 +18,18 @@ const UserPage = async ({ params }: UserPageProps) => {
     }
 
     const isFollowing = await isFollowingUser(user.id);
+    const isBlocked = await isBlockedByUser(user.id);
+
+    /* if (isBlocked) {
+        notFound();
+    } */
 
     return (
         <div className="flex flex-col gap-y-4">
             <p>Username : {user.username}</p>
             <p>User ID : {user.id}</p>
             <p>Is Following : {`${isFollowing}`}</p>
+            <p>Is Blocked by this user: {`${isBlocked}`}</p>
             <Actions isFollowing={isFollowing} userId={user.id} />
         </div>
     );
